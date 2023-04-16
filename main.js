b4792d960a301e646759de55704d5f25bf5f237c
Rivenshell Salomon <Rivenshell111@gmail.com>
2023-04-15 23:40:49 -0400

    therr js init

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import style from "./style.css";
 import Experience from "./Experience/Experience.js";
+import * as THREE from "three";
 
 const experience = new Experience(document.querySelector(".experience-canvas"));
